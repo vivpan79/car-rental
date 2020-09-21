a54e8de3f891d080d34e6846a97d05a9fc0db677
Vivek Pandit <vivek.pandit@axiomatics.com>
2020-09-21 19:07:04 +0200

    Add Spring boot stater to Maven pom

---
+++ src/test/java/com/infor/carrental/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.infor.carrental;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
