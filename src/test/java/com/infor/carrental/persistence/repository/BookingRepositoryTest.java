03813826979bd1fdf909997791ec581d23a071a9
Vivek Pandit <vivek.pandit@axiomatics.com>
2020-09-23 01:03:07 +0200

    Add Booking for car

---
+++ src/test/java/com/infor/carrental/persistence/repository/BookingRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.infor.carrental.persistence.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.infor.carrental.Application;
+import com.infor.carrental.persistence.entity.Booking;
+import com.infor.carrental.persistence.entity.Car;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
+public class BookingRepositoryTest {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Test
+    void givenBookingRepositoryWhenSaveAndRetrieveBookingEntityThenExactMatch() {
+        Car car = new Car();
+        car.setNumberPlate("ABC-123");
+        Car savedCar = carRepository.save(car);
+        Booking booking = new Booking();
+        LocalDateTime currentDataTime = LocalDateTime.now();
+        booking.setCar(savedCar);
+        booking.setFromDate(currentDataTime);
+        booking.setToDate(currentDataTime.plusMinutes(1L));
+        Booking savedBooking = bookingRepository.save(booking);
+        List<Booking> availabilities = bookingRepository.findAll(Example.of(savedBooking));
+        assertFalse(availabilities.isEmpty());
+        assertEquals(currentDataTime, availabilities.get(0).getFromDate());
+        assertEquals(currentDataTime.plusMinutes(1L), availabilities.get(0).getToDate());
+        assertEquals("ABC-123", availabilities.get(0).getCar().getNumberPlate());
+    }
+}
